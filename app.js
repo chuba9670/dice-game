b25788e6dcdd405676416544b138d47c6ec019cc
Chuba <Chuba967013@gmail.com>
2020-04-10 20:57:35 +0800

    roll dice bolgoloo click

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Шооны аль талаараа буусныг харуулах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-window.document.querySelector("#score-0").textContent = "0 ";
-window.document.querySelector("#score-1").textContent = "0 ";
-document.querySelector("#current-0").textContent = "0 ";
-document.querySelector("#current-1").textContent = "0 ";
-document.querySelector(".dice").style.display = "none";
 
-console.log("Shoo :" + dice);
+//Програм эхлэхэд бэлтгэе
+window.document.getElementById("score-0").textContent = "0 ";
+window.document.getElementById("score-1").textContent = "0 ";
+document.getElementById("current-0").textContent = "0 ";
+document.getElementById("current-1").textContent = "0 ";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.src = "dice-" + diceNumber + ".png";
+  diceDom.style.display = "block";
+});
