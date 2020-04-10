7f025cb726a5f45cba09c3ea5521b68bc77b0d80
Chuba <Chuba967013@gmail.com>
2020-04-10 21:58:22 +0800

    toglogchig solih current 0 bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -16,9 +16,32 @@ document.getElementById("current-0").textContent = "0 ";
 document.getElementById("current-1").textContent = "0 ";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-document.querySelector(".btn-roll").addEventListener("click", function () {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-  diceDom.src = "dice-" + diceNumber + ".png";
+//shoog shideh event listener
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  //1-6 dotorh sanamsargui toog gargaj awna
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // shoonii zurgiig gargaj irne
   diceDom.style.display = "block";
+  //buusan sanamsargui toond hargalzah shoonii zurgiig gargaj irne
+  diceDom.src = "dice-" + diceNumber + ".png";
+  // buusan too n 1 ylgaatai bol current deer nemne
+  if (diceNumber !== 1) {
+    //1 ees ylgaatai too buulaa, tuhain toog toglogchid nemj ogno
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 tei tentsu bwl nogoo toglogch ruu shiljine current 0 bolno
+    //tuhain toglolton deer tsugluulsan onoo 0 bolno current n
+    document.getElementById("current-" + activePlayer).textContent = "0";
+    roundScore = 0;
+    // herew toglogch ni 0 bwl toglogchiig 1 bolgo
+    // ugui bol 0 bolgo
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //ulaan tsegiig shiljuuleh
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+  }
 });
